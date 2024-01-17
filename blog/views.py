a73000db5d150a302f120338e3fa87bf6cfb65c4
JesusGPro <145535842+JesusGPro@users.noreply.github.com>
2024-01-17 10:12:14 +0300

    Plantilla modificada para mostrar los post guardados

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+# views concta modelos con plantillas, para ellos necesitamos los QuerySets (variable posts)
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(publishing_date__lte=timezone.now()).order_by('publishing_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+# request: todo lo que recibimos del usuario por internet
+# {'posts': posts} es un lugar ene el que podemos agregar algunas cosas para que la plantilla las use
+# lo nombramos de momento 'posts'
 
 
